0daab01158bc1ef30e555ae9bf70c94de48eca59
colinc1994 <32180839+colinc1994@users.noreply.github.com>
2024-06-15 16:10:39 -0400

    update

---
+++ scripts/P01 - read and clean data.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+library(readxl)
+library(data.table)
+rm(list = ls())
+## NOTE: 1990-2000
+# HAD TO DELETE DUPLICATE COLUMN NAMES ON DELEWARE, 
+# THEY WERE NOT NEEDED, NOTHING IS THERE
+# DELETED OLD STATE VARIABLE IN TEXAS BECAUSE IT WAS EMPTY
+
+## going to do this one at a time, there is probably a better way but it's three 
+# chunks so this shouldn't be bad
+
+
+######################## 1990 - 2000
+# get sheet names
+sheets_temp = readxl::excel_sheets(file.path("Data",
+                                             list.files("Data/",
+                                                               pattern = "1990-2000")[1]))
+# read in data and combine rows
+zc_change_90_00 = lapply(sheets_temp,function(i){
+temp = read_excel(file.path("Data",list.files("Data/",pattern = "1990-2000")[1]),
+                  sheet = i,)
+temp = temp %>% na.omit()
+colnames(temp) = colnames(temp) %>% make.names()
+temp = temp %>% 
+  mutate(across(everything(), as.character),
+         State = i)
+
+temp %>%  return()
+}) %>% 
+  do.call(bind_rows,.)
+
+######################### 2000-2010
+# get sheet names
+sheets_temp = readxl::excel_sheets(file.path("Data",
+                                             list.files("Data/",
+                                                        pattern = "2000-2010")[1]))
+# read data
+zc_change_00_10 = lapply(sheets_temp,function(i){
+  temp = read_excel(file.path("Data",list.files("Data/",pattern = "2000-2010")[1]),
+                    sheet = i,)
+  temp = temp %>% na.omit()
+  colnames(temp) = colnames(temp) %>% make.names()
+  temp = temp %>% 
+    mutate(across(everything(), as.character),
+           State = i)
+  
+  temp %>%  return()
+}) %>% 
+  do.call(bind_rows,.)
+
+######################## 2010 - 2020
+# get sheet names
+sheets_temp = readxl::excel_sheets(file.path("Data",
+                                             list.files("Data/",
+                                                        pattern = "2010-2020")[1]))
+# read data
+zc_change_10_20 = lapply(sheets_temp,function(i){
+  temp = read_excel(file.path("Data",list.files("Data/",pattern = "2010-2020")[1]),
+                    sheet = i,)
+  temp = temp %>% na.omit()
+  colnames(temp) = colnames(temp) %>% make.names()
+  temp = temp %>% 
+    mutate(across(everything(), as.character),
+           State = i)
+  
+  temp %>%  return()
+}) %>% 
+  do.call(bind_rows,.)
+
+# check the column names, they really should all be the same
+zc_change_00_10 %>% colnames()
+zc_change_10_20 %>% colnames()
+zc_change_90_00 %>% colnames()
+# checking the column names, there is a variable that should be remove in the 1990 data
+zc_change_90_00 = zc_change_90_00 %>% 
+  select(-...10)
+
+# I think it is safe now to smart bind everything
+zc_change_df = list(zc_change_00_10,
+     zc_change_10_20,
+     zc_change_90_00) %>% 
+  do.call(bind_rows,.) %>% 
+  mutate(New.County = ifelse(is.na(New.County),County,New.County), # the new county variable was just county for some states
+         State = State %>% gsub("-","",.)) # need to remove - which was in there for some reason
+
